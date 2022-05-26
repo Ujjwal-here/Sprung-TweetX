a683d5c7a221098b91d6732a4c61228998dac931
Ujjwal-here <ujjwalsbx@gmail.com>
2022-05-26 13:12:12 +0530

    Add Navbar navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
-import './App.css';
-import Signup from './components/Signup';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Main from "./components/Main";
+import Feed from "./components/Feed";
+import Users from "./components/Users";
 
 function App() {
   return (
-    <div className="App">
-      <Signup/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />}>
+          <Route path="" element={<Feed />} />
+          <Route path="users" element={<Users />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between px-40 py-4 shadow-md ">
+    <nav className="flex flex-row justify-between px-40 py-4 shadow-md ">
       <h1 className="font-bold text-2xl text-[#ff748d]">TweetX</h1>
       <div className="flex flex-row">
-        <h1 className="mx-12 font-bold text-lg text-[#e8e8e8]">Feed</h1>
-        <h1 className="mx-12 font-bold text-lg text-[#e8e8e8]">Users</h1>
-        <h1 className="ml-12 mr-20 font-bold text-lg text-[#e8e8e8]">Profile</h1>
+        <NavLink to='' className="mx-12 font-bold text-lg text-[#e8e8e8]">Feed</NavLink>
+        <NavLink to='users' className="mx-12 font-bold text-lg text-[#e8e8e8]">Users</NavLink>
+        <NavLink to='profile' className="ml-12 mr-20 font-bold text-lg text-[#e8e8e8]">Profile</NavLink>
       </div>
-    </div>
+    </nav>
   );
 };
 
