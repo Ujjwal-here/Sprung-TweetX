ce8dfc1dbe62bd96db75622d7021b0e0e9f79731
Ujjwal-here <ujjwalsbx@gmail.com>
2022-05-27 15:16:33 +0530

    change font size of navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <nav className="flex flex-row justify-between px-40 py-4 shadow-md ">
       <h1 className="font-bold text-2xl text-[#ff748d]">TweetX</h1>
       <div className="flex flex-row">
-        <NavLink to='' className="mx-12 font-bold text-lg text-[#e8e8e8]">Feed</NavLink>
-        <NavLink to='users' className="mx-12 font-bold text-lg text-[#e8e8e8]">Users</NavLink>
-        <NavLink to='profile' className="ml-12 mr-20 font-bold text-lg text-[#e8e8e8]">Profile</NavLink>
+        <NavLink to='' className="mx-12 my-2 font-semibold text-md text-[#e8e8e8]">Feed</NavLink>
+        <NavLink to='users' className="mx-12 my-2 font-semibold text-md text-[#e8e8e8]">Users</NavLink>
+        <NavLink to='profile' className="ml-12 my-2 mr-20 font-semibold text-md text-[#e8e8e8]">Profile</NavLink>
       </div>
     </nav>
   );
