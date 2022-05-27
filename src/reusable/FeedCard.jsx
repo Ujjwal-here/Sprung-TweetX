99de6a0d8962cc70a12df468bbb66488de409a46
Ujjwal-here <ujjwalsbx@gmail.com>
2022-05-27 17:02:33 +0530

    Add responsiveness

--- src/reusable/FeedCard.jsx
+++ src/reusable/FeedCard.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const FeedCard = (props) => {
   return (
     <div
-      className={`pl-8 py-8 mx-16 flex flex-row  my-12 rounded-3xl main ${props.shadow}`}
+      className={`sm:pl-6 sm:py-8 md:pl-8 lg:pl-8 lg:py-8 mx-16 flex flex-row  my-12 rounded-3xl main ${props.shadow}`}
     >
       <div className={`border border-[#b8b8b8] rounded-full ${props.h} ${props.w}`}></div>
       <div className="flex flex-col">
-        <h1 className="mx-8 text-lg font-semibold text-[#707070]">
+        <h1 className="mx-8 sm:text-lg lg:text-xl font-semibold text-[#707070]">
           Arjun Reddy
         </h1>
         <h1 className="mr-24 mb-2 text-xs text-[#bfbfbf] place-self-end">
           10 mins ago
         </h1>
-        <h1 className={`ml-8 mr-20 ${props.desfont} text-[#b6b6b6]`}>
+        <h1 className={`ml-8 mr-20 sm:text-xs lg:text-sm ${props.desfont} text-[#b6b6b6]`}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
           lectus tortor, aliquet sed lectus eget, iaculis egestas erat. Nam non
           lacinia urna, a luctus leo. Morbi vestibulum lorem porttitor massa
