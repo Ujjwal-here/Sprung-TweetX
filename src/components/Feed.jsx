60dfd34da459032cde56d8f6e2fc232ce4127414
Ujjwal-here <ujjwalsbx@gmail.com>
2022-05-27 17:02:43 +0530

    Add responsiveness

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,8 +3,8 @@ import FeedCard from "../reusable/FeedCard";
 
 const Feed = () => {
   return (
-    <div className="mx-96">
-      <button className="mt-8 py-2 px-6 mx-20 rounded bg-[#ff748d] text-white ">
+    <div className="sm:mx-14 lg:mx-32 md:mx-20 2xl:mx-96">
+      <button className="mt-8 py-2 px-6 mx-20 rounded sm:text-xs lg:text-md bg-[#ff748d] text-white ">
         Write
       </button>
       <FeedCard shadow="shadow-lg" desfont="text-sm" h="h-16" w="w-44"/>
