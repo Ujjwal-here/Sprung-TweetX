782a76d260e991819112aa9f40d9a881f50874f5
Ujjwal-here <ujjwalsbx@gmail.com>
2022-05-27 15:17:50 +0530

    Add margin and padding

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,23 +1,13 @@
 import React from "react";
+import Follow from "../reusable/Follow"
+import Following from "../reusable/Following"
 
 const Users = () => {
   return (
     <div className="mx-96 my-10 ">
-      <div className="flex flex-row items-center">
-        <div className="border border-[#b8b8b8] rounded-full h-14 w-14"></div>
-        <div className="flex flex-col">
-          <h1 className="mx-8 text-lg font-semibold text-[#707070]">
-            Arjun Reddy
-          </h1>
-          <h1 className="mx-8 my-2 align-middle text-xs text-[#bfbfbf]">
-            Following:200
-          </h1>
-        </div>
-        <button className="mx-48 my-6 py-2 px-6 rounded bg-[#ff748d] text-white ">
-          Follow
-        </button>
-      </div>
-      <hr className="mr-60 my-4"/>
+      <Follow/>
+      <hr className="mx-28"/>
+      <Following/>
     </div>
   );
 };
